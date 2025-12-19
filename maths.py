9156bd48563cab5862e38df3fc18c775ca5c2749
aadilhamdan818-dot <aadilhamdan818@gmail.com>
2025-12-19 13:14:02 +0100

    Add files via upload

---
+++ maths.py
@@ -0,0 +1,13 @@
+operation = ("enter operation (+, -, *, /): ")
+num1 =int (input ("enter the first number:"))
+num2 =int (input("enter the second number:"))
+if operation=="+":
+    input( num1 + num2)
+elif operation=="-":
+    input ( num1 - num2)
+elif operation=="*":
+    input ( num1 * num2)
+else:
+    input ( num1 / num2)
+
+
